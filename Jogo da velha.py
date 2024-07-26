4c11f11b4f5c4618c9db2175c7c635ee632c82c4
brunokimurats <b282745@dac.unicamp.br>
2024-07-26 12:13:10 -0300

    jv.interface()
    Etapa inicial da interface
    Correção de bug das cores

--- Jogo da velha.py
+++ Jogo da velha.py
@@ -2,34 +2,67 @@
 
 import tkinter
 
+def set_tile(row, column):
+    pass
 
-jog_X = 'X'
-jog_O = 'O'
-inicio = input(f'Quem irá começar? ')
-if inicio in'Xx':
-    jog_atual = jog_X
-    print('X')
-elif inicio in 'oO0':
-    jog_atual = jog_X
-    print('O')
+def new_game():
+    pass
+
+
+cor_1 = 'RoyalBlue2'
+cor_2 = 'yellow2'
+cor_3 = 'PeachPuff3'
+cor_4 = 'dark slade blue'
 
 tabuleiro = [[0, 0, 0],
              [0, 0, 0],
              [0, 0, 0]]
 
-cor_1 = '#252f9c'
-cor_2 = 'cfe5ff'
-cor_3 = '7c1034'
-cor_4 = 'd1dfbb'
+jog_X = 'X'
+jog_O = 'O'
+jog_atual = jog_X
+#inicio = input(f'Quem irá começar? ')
+#if inicio in'Xx':
+#    jog_atual = jog_X
+#    print('X')
+#elif inicio in 'oO0':
+#    jog_atual = jog_X
+#    print('O')
+
 
 window = tkinter.Tk()
 window.title('Jogo da Velha')
 window.resizable(False, False)
 
 frame = tkinter.Frame(window)
-label = tkinter.Label(frame, text='Turno de '+jog_atual, font=('Consolas', 20),
+label = tkinter.Label(frame, text='Turno de '+jog_atual, font=('fixedsys', 20),
                        background = cor_3, foreground='white')
-label.pack()
+label.grid(row=0, column=0, columnspan=3, sticky='we')
 
+for row in range(3):
+    for column in range(3):
+        tabuleiro[row][column] = tkinter.Button(frame, text='', font=('fixedsys', 50, 'bold'),
+                                                background= cor_3, foreground=cor_1, width=4, height=1,
+                                                command=lambda row=row, column=column: set_tile(row, column))
+        tabuleiro[row][column].grid(row = row + 1, column= column)
+
+reiniciar = tkinter.Button(frame, text='Reiniciar', font=('fixedsys', 20), 
+                           background=cor_3, foreground='white',
+                           command=new_game)
+reiniciar.grid(row=4, column=0, columnspan=3, sticky='we')
+
+
+frame.pack()
+
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenmmwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f'{window_width}x{window_height}+{window_x}+{window_y}')
 
 window.mainloop()
